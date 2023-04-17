7eeab6a8feba7fa2718f75962862f730385fa1fe
FrozanL1ke <112868311+FrozanL1ke@users.noreply.github.com>
2023-04-17 15:23:29 +0400

    task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,37 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+
+
+    let d = b ** 2 - 4 * a * c;
+  if (d < 0) {
+    return [];
+  } else if (d === 0) {
+    let x = -b / (2 * a);
+    return [x];
+  } else {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+    return [x1, x2];
+  }
 }
 
+
+
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let percentNumber = Number(percent);
+  let contributionNumber = Number(contribution);
+  let amountNumber = Number(amount);
+  let countMonthsNumber = Number(countMonths);
+
+  if (isNaN(percentNumber) || isNaN(contributionNumber) || isNaN(amountNumber) || isNaN(countMonthsNumber)) {
+    return false;
+  }
+
+  let monthlyPercent = percentNumber / 100 / 12;
+  let creditBody = amountNumber - contributionNumber;
+  let monthlyPayment = creditBody * (monthlyPercent + monthlyPercent / ((1 + monthlyPercent) ** countMonthsNumber - 1));
+  let totalAmount = monthlyPayment * countMonthsNumber;
+
+  return parseFloat(totalAmount.toFixed(2));
 }
