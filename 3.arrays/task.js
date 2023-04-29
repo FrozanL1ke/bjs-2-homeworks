cfa4636dbeb618c13bb6525605645674ebe58c51
FrozanL1ke <112868311+FrozanL1ke@users.noreply.github.com>
2023-04-29 16:52:11 +0400

    task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,10 @@
 function compareArrays(arr1, arr2) {
-  
-}
+    return arr1.every((value, index) => value === arr2[index]) && arr2.every((value, index) => value === arr1[index]);
+  }
 
-function getUsersNamesInAgeRange(users, gender) {
-  
-}
+  function getUsersNamesInAgeRange(users, gender) {
+    const filteredUsers = users.filter(user => user.gender === gender);
+    const totalAge = filteredUsers.reduce((acc, user) => acc + user.age, 0);
+    const averageAge = filteredUsers.length ? totalAge / filteredUsers.length : 0;
+    return averageAge;
+  }
